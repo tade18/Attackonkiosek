b7dee4e6ac346b01213393b31da847ac39c47600
tade18 <tade.18@seznam.cz>
2024-01-24 10:24:20 +0100

    update

--- res/js/characters/Character.js
+++ res/js/characters/Character.js
@@ -1,37 +1,79 @@
-
 export class Character {
-    constructor(name, hp, dmg, speed, type){
+    /*static - dana vec nalezi/patri tride - ne objektu*/
+    static charactersData;
+
+    /*
+    const foo = new Character(....);
+    foo.name; - name patri objektu
+
+    pokud dana vec patri tride - je static
+    Character.charactersData - zapis vypada takto
+    foo.charactersData - nebude fungovat
+    */
+
+//CONSTRUCTOR------------------------------------------
+    constructor(name, hp, dmg, speed, type) {
         this.name = name;
-        this.hp = hp;
-        this.dmg = dmg;
-        this.speed = speed;
         this.img = new Image();
-        this.setType(type);
+        this.setType(name);
         this.img.src = this.path;
         this.ratio = 0.3;
         this.size = {
             width: 336 * this.ratio,
             height: 634 * this.ratio
         };
-        this.position = {
-            x: 100,
-            y: 300
-        };
+        this.state = 0;
+        this.animationSpeed = 4;
+        this.frame= {
+            counter: 0,
+            index: 1,
+            maxIndex: 11,
+            width: 100;
+            height: 100
+        }
     }
-    setType(type){
-            const characterTypes = [
-                "./res/img/fraftik4brady.png"
-            ]
-            this.path = characterTypes[type];
+//SETTYPE-----------------------------------------------
+    setType(name) {
+        Character.charactersData.map((obj)=>{
+            if(name  === obj.name){
+                this.sprites = obj.sprites;
+                this.hp = obj.stats.hp;
+                this.maxHp = this.hp;
+                this.dmg = obj.stats.dmg;
+                this.speed = obj.stats.speed;
+                this.side = obj.stats.side;
+                this.velocity = {
+                    x: obj.stats.velocity * this.speed;
+                };
+                this.position{
+                    x: obj.stats.position,
+                    y: 350,
+                };
+                return;
+            }
+        })
     }
-
-    draw(ctx){
-        ctx.drawImage(this.img, this.position.x, this.position.y, this.size.width, this.size.height);
+//ANIMACE-----------------------------------------------
+    animate(ctx){
+        ctx.drawImage
     }
-    update(state){
-        switch(state){
+//VYKRESLENÍ--------------------------------------------
+    draw(ctx) {
+        ctx.save();
+        if (this.side === 0) {
+            this.animate(ctx);
+            return ctx.restore();
+        }
+            ctx.translate(this.position.x + this.size.width, 0)
+            ctx.scale(-1, 1);
+            this.animate(ctx);
+            ctx.drawImage(this.img, 0, this.position.y, this.size.width, this.size.height); 
+            ctx.restore();
+        }
+    update(state) {
+        switch (state) {
             case 0:
-                this.position.x++;
+                this.move()
                 break;
             case 1:
                 console.log(this.name + "attacks!")
@@ -39,12 +81,25 @@ export class Character {
             case 2:
                 console.log(this.name + " has found peace in this war! He lost the fight but he will never be forgotten!")
                 this.position.x = 0;
-                this.hp = 10000;
+                this.hp = 1000;
                 break;
             default:
         }
     }
+//POHYB-------------------------------------------------
+    move() {
+        this.position.x += this.velocity.x;
+        if (this.position.x >= 1100) {
+            this.velocity.x *= -1;
+            this.side = 1;
+        }
+        if (this.position.x <= 90) {
+            this.velocity.x *= -1;
+            this.side = 0;
+        }
+    }
 }
+
 //funkce - stojí sama o sobě
 //metoda - funkce, ale patrí nějakému objektu
 //kopie z šablony - objekt (object) - instance
--- res/js/main.js
+++ res/js/main.js
@@ -1,11 +1,22 @@
+//IMPORTY------------------------------------------
 import { Character } from "./characters/Character.js";
 import { Background } from "./ui/basic-utils.js";
-const Frafta = new Character("Frafta", 10000, 1, 0.1, 0);
+
+//CHARACTERS---------------------------------------
+/*Postavy pro hráče*/
+const friendly = [];
+/*Postavy pro pc*/
+const enemy = [];
+
+//POZADÍ-------------------------------------------
 const background = new Background();
 console.log(background);
+
+//CANVAS-------------------------------------------
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-//KLÁVESNICE
+
+//KLÁVESNICE---------------------------------------
 const keys = {};
 document.addEventListener("keydown", (e) => {
     keys[e.code] = true;
@@ -13,6 +24,8 @@ document.addEventListener("keydown", (e) => {
 document.addEventListener("keyup", (e) => {
     keys[e.code] = false;
 });
+
+//RESIZE CANVAS------------------------------------
 const resizeCanvas = () => {
     canvas.width = 1280;
     canvas.height = 720;
@@ -20,10 +33,14 @@ const resizeCanvas = () => {
 const clearCanvas = () => {
     background.draw(ctx);
 };
+
+//UPDATE-------------------------------------------
 const update = () => {
-    if(Frafta.position.x >= 640){
+    Frafta.update(0);
+    unrealurbic.update(0);
+    /*if(Frafta.position.x >= 640){
         Frafta.update(1);
-        Frafta.hp-=100;
+        //Frafta.hp-=100;
         console.log(Frafta.hp)
         if(Frafta.hp <= 0){
             Frafta.update(2);
@@ -31,11 +48,22 @@ const update = () => {
     }
     else{
         Frafta.update(0);
-    }
+    }*/
 };
-const render = () => {Frafta.draw(ctx)};
+
+//RENDER-----------------------------------------
+const render = () => {
+    /*a = postava která je v poli nařadě*/
+        friendly.map((a)=>{
+            a.draw(ctx);
+        })
+        enemies.map((a)=>{
+            a.draw(ctx);
+        })
+};
 const getFps = () => {};
-//LOOP
+
+//LOOP-------------------------------------------
 const gameLoop = () =>{
     //resizeCanvas
     resizeCanvas();
@@ -49,28 +77,72 @@ const gameLoop = () =>{
     getFps();
     //zažádáme si snímek z funkce gameloop
     window.requestAnimationFrame(gameLoop);
+};
+
+//NAČTENÍ DAT Z CHARACTERS.JSON------------------
+const loadData = async()=>{
+    const file = await fetch("./res/data/characters.json");
+    const data = await file.json();
+    Character.charactersData = data;
+};
+
+//PRERENDER--------------------------------------
+const prerender = () => {
+    friendly.push(new Character("UnrealUrbic"));
+    enemies.push(new Character("Frafticek"));
 }
 
-
-//WINDOWS.ONLOAD = po načtení stránky
-window.onload = () => {
+//WINDOWS.ONLOAD---------------------------------
+window.onload = async() => {
+    /*načteme soubory*/
+    await loadData();
+    /*prerenderujeme postavy*/
+    prerender();
+    /*spustime hru*/
     window.requestAnimationFrame(gameLoop);
-}
+};
 
 
-//ARRAY
-//indexování        0         1       2        3    4   5   6
-/*const names = ["Karel", "Ondřej", "Jan" , "Petr", 1 , 2 , 3];
-console.log(names[3]); vypíše ,,Petr"
-names[3] = "Viktor";  změní Petr na Viktor*/
-
-//Js Objekt
-/*const urban = {
-    hp: 1000,
-    dmg: 1
-    as: 0.01,
-    speed: 0.1,
-}
-console.log(urban.hp);
-urban.hp -=50;
-console.log(urban.hp);*/
+//                                   .:;it1i;:,,..
+//                                .:i1tfffLLLf1i;,,,.
+//                              .:;;i1tffLfLLCLti:,,,,.
+//                            .,,,:iLLLLLffffftti;:,,,,.
+//                            :,,,;11i;;i11ii:,,,,,,,,,,,
+//                           ,:..,:;,.,,,:;;::,...,,:,,.,.
+//                           ;,..,,,:tf1;;;::::::::;;:,..,
+//                           ,....,;fLLCCCCLt111tt1;;;,,.,
+//                           ....,i1ttffLCCCLfffft1;;::,.,
+//                           ....,i11ttfLLLLfffftt1;;:::.,
+//                           ::..,;tt1tffLCLLLffffti::::,,
+//                           ;i,.:i1ttt1ttffLLfLL1:,,,,,:.
+//             .1i:      .....11:;11i;;;::,:;1tt;,,::,,::
+//             ;0Ct1:..,Lif1,:ifLf11ii11ii;:;ift::;;;;;;:
+//              :t1ti::,:,::,,,iLfLftt1t1ttfCLCC;i1ttf1i:
+//               ;11ii:;ii1;f0Ci11fiifLttGGCLLLG1;;i11i;,
+//               1Lt1111i...;t1::::,,iLftf1itttf1:::::::.
+//             .iC0GGG00C:...  :1;Gi,,iii;itti;;:::,:;;:
+//             tftft1iii;:,:;1tfC;:;:;1t1tttttft1;::;::.
+//             .1fti;i1ttfLLCG00CffCt,:;;i1ffft1ii;;,,,
+//             .LGCffffffffttffftti:,..,,,:i1tfft1;,;;..
+//            .;L1tftt1iii111iii:.......,i:,,::::,,:i;...
+//           ..Lt:1fLfft11it1,;: ........;t,........ .......
+//          .,.G1,i;;;i11ii:.,,,...........,...... ...... ..,..
+//         ,,..GG,;;;::;:;i..,:,................,::,..............
+//        ,....i@1,;111;it, .,::..............;fLLtf1,...............
+//       ,..... t@;:1iiGG, ...:: .............,f00GGC; ................
+//      ,....... L8:.18G, ...it1,...............t880G1..................
+//     ,..........8L1@C.,;iiitt1iiii:........... t808f ..................
+//    ............,:;i..,,::,:,,,,................f08t ...................
+//    ,...........    ...  ........................fG; ...............  .,
+//   ..............................................,i.................;, .,
+//   ,................................................................Lt...,
+//  ,............................................................... ;i:...,,
+// .,.................................................................  ....,,
+// ,.........................................................................,.
+// ,..........................................................................,
+// ...........................................................................,.
+//   ..........................................................................,
+//       ......,. ..............................................................,
+//              ,................................................................,
+//               ,...............................................................,.
+//                , ..............................................................,
